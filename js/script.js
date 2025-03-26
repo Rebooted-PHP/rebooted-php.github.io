4fbcfb2aac6baef43b30ae79a67882bc745f9b9f
Jan Kristian Valgijainen <54046100+ogchibi95@users.noreply.github.com>
2025-03-26 09:29:12 +0200

    Added.

--- js/script.js
+++ js/script.js
@@ -5,10 +5,9 @@
 
 function import_script(let URI){
 
-    if(!empty(URI)){
         var script_tag = document.createElement("script");
         script_tag.src = URI;
         script_tag.async = false;
         document.head.appendChild(script_tag);
-    }
+    
 }
